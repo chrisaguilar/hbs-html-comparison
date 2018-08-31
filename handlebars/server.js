e6126908b28b4bde0491d5146eb6f467d5150a28
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-08-31 02:17:08 -0400

    Cleaned up server files

--- handlebars/server.js
+++ handlebars/server.js
@@ -5,11 +5,14 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 
+const apiRoutes = require('./routes/api-routes');
+const handlebarsRoutes = require('./routes/handlebars-routes');
 const models = require('./db/models');
 
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: path.join(app.get('views'), 'layouts/main') }));
@@ -17,14 +20,14 @@ app.engine('handlebars', exphbs({ defaultLayout: path.join(app.get('views'), 'la
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(require('./routes/api-routes'));
-app.use(require('./routes/handlebars-routes'));
+app.use(apiRoutes);
+app.use(handlebarsRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 models.sequelize
     .sync({ force: false })
     .then(() => {
         console.log('Database Synchronized');
-        app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     })
     .catch(console.error);
--- html/server.js
+++ html/server.js
@@ -4,23 +4,25 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
 const models = require('./db/models');
 
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(require('./routes/api-routes'));
-app.use(require('./routes/html-routes'));
+app.use(apiRoutes);
+app.use(htmlRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 models.sequelize
     .sync({ force: false })
     .then(() => {
         console.log('Database Synchronized');
-        app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     })
     .catch(console.error);
